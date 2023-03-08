02160c240088a64b9a36f4b42daecefe24af72f4
Glasena <alisonglasenapp@gmail.com>
2023-03-08 13:01:04 -0300

    Adicionado Dependencias e Model de User

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,2 @@
+import "reflect-metadata"
 // express configuration
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-//server configuration
+import { DataSource } from "typeorm";
+import { User } from "./database/entities/User";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "test",
+    password: "test",
+    database: "test",
+    synchronize: true,
+    logging: true,
+    entities: [User],
+    subscribers: [],
+    migrations: [],
+})
